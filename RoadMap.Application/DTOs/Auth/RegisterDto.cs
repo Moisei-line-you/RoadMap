9762cddb6df6f57d5488b7bf7f0e7151024d9874
arcb8 <arcb8@gmail.com>
2026-05-06 20:23:20 +0300

    add ConfirmPassword in DTO

--- RoadMap.Application/DTOs/Auth/RegisterDto.cs
+++ RoadMap.Application/DTOs/Auth/RegisterDto.cs
@@ -12,4 +12,7 @@ public class RegisterDto
     [Required]
     [MinLength(8)]
     public string Password { get; set; } =  string.Empty;
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
